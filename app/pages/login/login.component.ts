8d0ab8cb0cb00ff0fbfb8fec0f48059984a3bb15
jgli <jgli@pcgus.com>
2017-01-03 13:05:49 -0500

    chapter 6

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -4,7 +4,9 @@ import { User } from "../../shared/user";
 import { Color } from "color";
 import { View } from "ui/core/view"
 import { UserService } from "../../shared/user.service";
-import { Page } from "ui/page"
+import { Page } from "ui/page";
+import { setHintColor } from "../../shared/hint-util";
+import { TextField } from "ui/text-field";
 
 
 @Component({
@@ -18,6 +20,8 @@ export class LoginComponent implements OnInit {
     email = "nativescriptrocks@telerik.com";
     isLoggingIn = true;
     @ViewChild("container") container: ElementRef;
+    @ViewChild("email") elEmail: ElementRef;
+    @ViewChild("password") elPassword: ElementRef;
 
     constructor(private page: Page, private router: Router, private userService: UserService) {
         this.user = new User();
@@ -29,19 +33,20 @@ export class LoginComponent implements OnInit {
         this.page.actionBarHidden = true;
         this.page.backgroundImage = "res://bg_login";
     }
-    
+
     toggleDisplay(): void {
         this.isLoggingIn = !this.isLoggingIn;
+        this.setTextFieldColors();
         let cont = <View>this.container.nativeElement;
         cont.animate({
-            backgroundColor:this.isLoggingIn?new Color("white"):new Color('#301217'),
-            duration:200
+            backgroundColor: this.isLoggingIn ? new Color("white") : new Color('#301217'),
+            duration: 200
         });
     }
 
 
     public submit() {
-        if(!this.user.isValidEmail()){
+        if (!this.user.isValidEmail()) {
             alert('Please enter a valid email address');
             return;
         }
@@ -71,5 +76,18 @@ export class LoginComponent implements OnInit {
             },
             () => alert("We couldn't create your account!")
             );
+    }
+
+    private setTextFieldColors() {
+        let emailTextField = <TextField>this.elEmail.nativeElement;
+        let passwordTextField = <TextField>this.elPassword.nativeElement;
+
+        let mainTextColor = new Color(this.isLoggingIn ? 'black' : '#C4AFB4');
+        emailTextField.color = mainTextColor;
+        passwordTextField.color = mainTextColor;
+
+        let hintColor = new Color(this.isLoggingIn ? '#ACA6A7' : '#C4AFB4');
+        setHintColor({ view: emailTextField, color: hintColor });
+        setHintColor({ view: passwordTextField, color: hintColor });
     }
 }
